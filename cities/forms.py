93c401e16978e7de5dd7776a1cb8a8a1a7b022a6
dpris <dmitry@snov.io>
2022-12-06 21:24:52 +0200

    030 form ModelForm

---
+++ cities/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from cities.models import City
+
+
+class HtmlForm(forms.Form):
+    name = forms.CharField(label='City')
+
+
+class CityForm(forms.ModelForm):
+    class Meta:
+        model = City
+        fields = ('name',)
