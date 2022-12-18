de3b0a711804ac7948784da9701f918206200d61
dpris <dmitry@snov.io>
2022-12-18 23:45:16 +0200

    051 form to find routes

---
+++ routes/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from cities.models import City
+
+
+class RouteForm(forms.Form):
+    from_city = forms.ModelChoiceField(label='From', queryset=City.objects.all(), widget=forms.Select(
+        attrs={
+            'class': 'form-control',
+        }))
+    to_city = forms.ModelChoiceField(label='To', queryset=City.objects.all(), widget=forms.Select(
+        attrs={
+            'class': 'form-control',
+        }))
+    cities = forms.ModelMultipleChoiceField(label='Through the cities', queryset=City.objects.all(),
+                                            required=False, widget=forms.SelectMultiple(
+            attrs={
+                'class': 'form-control',
+            }))
+    travelling_time = forms.IntegerField(label='Travel time', widget=forms.NumberInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Travel time'
+    }))
