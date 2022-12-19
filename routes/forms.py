d4c68f0c80dff97722f37d890c044d281330219a
dpris <dmitry@snov.io>
2022-12-19 22:01:13 +0200

    052 update fun form search - select2.org

--- routes/forms.py
+++ routes/forms.py
@@ -6,16 +6,16 @@ from cities.models import City
 class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(label='From', queryset=City.objects.all(), widget=forms.Select(
         attrs={
-            'class': 'form-control',
+            'class': 'form-control js-example-basic-single',
         }))
     to_city = forms.ModelChoiceField(label='To', queryset=City.objects.all(), widget=forms.Select(
         attrs={
-            'class': 'form-control',
+            'class': 'form-control js-example-basic-single',
         }))
     cities = forms.ModelMultipleChoiceField(label='Through the cities', queryset=City.objects.all(),
                                             required=False, widget=forms.SelectMultiple(
             attrs={
-                'class': 'form-control',
+                'class': 'form-control js-example-basic-multiple',
             }))
     travelling_time = forms.IntegerField(label='Travel time', widget=forms.NumberInput(attrs={
         'class': 'form-control',
