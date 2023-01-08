37b63f51c338f9827a9d101da8f756a44d14a42e
dpris <dmitry@snov.io>
2023-01-08 13:30:17 +0200

    071 route detail

--- routes/views.py
+++ routes/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from cities.models import City
 from routes.forms import RouteForm, RouteModelForm
@@ -74,3 +74,8 @@ class RouteListView(ListView):
     paginate_by = 2
     model = Route
     template_name = 'routes/list.html'
+
+
+class RouteDetailView(DetailView):
+    queryset = Route.objects.all()
+    template_name = 'routes/detail.html'
