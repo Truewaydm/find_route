f5aa8746f4fdb1c03e3a9722b3c6fe834bb9cd78
dpris <dmitry@snov.io>
2023-01-19 17:38:46 +0200

    076 new user registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.hashers import check_password
 User = get_user_model()
 
 
-class UserLoginForm(AuthenticationForm):
+class UserLoginForm(forms.Form):
     username = forms.CharField(label='username', widget=forms.TextInput(attrs={
         'class': 'form-control',
         'placeholder': 'Input username'
@@ -29,3 +29,28 @@ class UserLoginForm(AuthenticationForm):
             if not user:
                 raise forms.ValidationError('This user is inactive')
         return super().clean(*args, **kwargs)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    username = forms.CharField(label='username', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Input username'
+    }))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Input password'
+    }))
+    password2 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Input password'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data['password'] != data['password2']:
+            raise forms.ValidationError('Passwords do not match')
+        return data['password2']
